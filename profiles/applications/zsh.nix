a8814a22e6175d51a472f434bc32bd72a9b17b7c
Ulli Goschler <git@nce.wtf>
2024-10-16 10:51:00 +0200

    add oh-my-posh setup

--- profiles/applications/zsh.nix
+++ profiles/applications/zsh.nix
@@ -12,13 +12,113 @@ with lib;
       fzf
       eza
       bat
+      ripgrep
+      fd
     ];
   };
 
   programs = {
+
     zoxide = {
       enable = true;
     };
+
+    oh-my-posh = {
+      enable = true;
+
+      settings = {
+
+        "version" = 2;
+        "final_space" = true;
+        "console_title_template" = "{{ .Shell }} in {{ .Folder }}";
+        "blocks" = [
+          {
+            "type" = "prompt";
+            "alignment" = "left";
+            "newline" = true;
+            "segments" = [
+              {
+                "type" = "path";
+                "style" = "plain";
+                "background" = "transparent";
+                "foreground" = "blue";
+                "template" = "{{ .Path }}";
+                "properties" = {
+                  "style" = "full";
+                };
+              }
+              {
+                "type" = "kubectl";
+                "style" = "plain";
+                "powerline_symbol" = "";
+                "foreground" = "yellow";
+                "background" = "transparent";
+                "template" = " ⎈ {{.Context}}{{if .Namespace}}  = = {{.Namespace}}{{end}}";
+              }
+              {
+                "type" = "git";
+                "style" = "plain";
+                "foreground" = "p =grey";
+                "background" = "transparent";
+                "template" = " {{ .HEAD }}{{ if or (.Working.Changed) (.Staging.Changed) }}*{{ end }} <cyan>{{ if gt .Behind 0 }}⇣{{ end }}{{ if gt .Ahead 0 }}⇡{{ end }}</>";
+                "properties" = {
+                  "branch_icon" = "";
+                  "commit_icon" = "@";
+                  "fetch_status" = true;
+                };
+              }
+            ];
+          }
+          {
+            "type" = "rprompt";
+            "overflow" = "hidden";
+            "segments" = [
+              {
+                "type" = "executiontime";
+                "style" = "plain";
+                "foreground" = "yellow";
+                "background" = "transparent";
+                "template" = "{{ .FormattedMs }}";
+                "properties" = {
+                  "threshold" = 5000;
+                };
+              }
+            ];
+          }
+          {
+            "type" = "prompt";
+            "alignment" = "left";
+            "newline" = true;
+            "segments" = [
+              {
+                "type" = "text";
+                "style" = "plain";
+                "foreground_templates" = [
+                  "{{if gt .Code 0}}red{{end}}"
+                  "{{if eq .Code 0}}magenta{{end}}"
+                ];
+                "background" = "transparent";
+                "template" = "❯";
+              }
+            ];
+          }
+        ];
+        "transient_prompt" = {
+          "foreground_templates" = [
+            "{{if gt .Code 0}}red{{end}}"
+            "{{if eq .Code 0}}magenta{{end}}"
+          ];
+          "background" = "transparent";
+          "template" = " ⎈ {{ .Segments.Kubectl.Context }} ❯ ";
+        };
+        "secondary_prompt" = {
+          "foreground" = "magenta";
+          "background" = "transparent";
+          "template" = "❯❯ ";
+        };
+      };
+    };
+
     zsh = {
       enable = true;
 
