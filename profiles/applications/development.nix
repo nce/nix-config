c6a168c3744ef1734a6f894ef274577070e8fcc8
Ulli Goschler <git@nce.wtf>
2024-10-16 10:51:10 +0200

    add k8s/k9s/go stuff

--- profiles/applications/development.nix
+++ profiles/applications/development.nix
@@ -9,8 +9,91 @@ with lib;
   home = {
     packages = with pkgs; [
       go
+      golangci-lint
+      go-critic
       cargo
       nixfmt-rfc-style
     ];
+  };
+
+  programs = {
+    k9s = {
+      enable = true;
+
+      plugin = {
+        plugins = {
+
+          stern = {
+            shortCut = "Shift-L";
+            confirm = false;
+            description = "Logs <Stern>";
+            scopes = [ "pods" ];
+            command = "stern";
+            background = false;
+            args = [
+              "--tail"
+              "50"
+              "$FILTER"
+              "-n"
+              "$NAMESPACE"
+              "--context"
+              "$CONTEXT"
+            ];
+          };
+
+          reconcile-hr = {
+            shortCut = "Shift-R";
+            confirm = false;
+            description = "Flux reconcile";
+            scopes = [
+              "helmreleases"
+            ];
+            command = "bash";
+            background = false;
+            args = [
+              "-c"
+              "flux --context $CONTEXT reconcile helmrelease -n $NAMESPACE $NAME |& less"
+            ];
+          };
+        };
+
+      };
+
+      settings = {
+        k9s = {
+          refreshRate = 2;
+          maxConnRetry = 5;
+          noExitOnCtrlC = false;
+          readOnly = false;
+          ui = {
+            enableMouse = false;
+            headless = false;
+            logoless = true;
+            crumbsless = false;
+            noIcons = false;
+          };
+          #  skipLatestRevCheck= false;
+          logger = {
+            tail = 100;
+            buffer = 5000;
+            sinceSeconds = 60;
+            fullScreenLogs = false;
+            textWrap = true;
+            showTime = false;
+          };
+          thresholds = {
+            cpu = {
+              critical = 90;
+              warn = 70;
+            };
+            memory = {
+              critical = 90;
+              warn = 70;
+            };
+          };
+
+        };
+      };
+    };
   };
 }
--- profiles/applications/kubernetes.nix
+++ profiles/applications/kubernetes.nix
@@ -15,6 +15,7 @@ with lib;
       kubernetes-helm
       dive
       stern
+      krew
     ];
   };
 
