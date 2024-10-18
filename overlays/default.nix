0fc1ef6576eeba1042fbb4ae484948cd3c0b7e00
Ulli Goschler <git@nce.wtf>
2024-10-18 21:11:40 +0200

    fix vim plugin

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,18 @@
 self: super:
 
 {
+    vimPlugins = self.lib.recursiveUpdate super.vimPlugins {
+    lsp-zero-nvim = self.vimUtils.buildVimPlugin {
+      pname = "lsp-zero-nvim";
+      version = "4.0";
+      src = self.fetchFromGitHub {
+        owner = "VonHeikemen";
+        repo = "lsp-zero.nvim";
+        rev = "19a8b90340c6c522219c84f7befe784d657ab28e";
+        hash = "sha256-tXllggyw7sm8zwOHrn+2mcTHch6Vp1u3BhjxvH+VCbA=";
+      };
+    };
+  };
+
     tourenbuchctl = super.callPackage ./tourenbuchctl { };
 }
