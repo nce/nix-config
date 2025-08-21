8025cfcae371afb4ec74ba1aef79793379132678
Ulli Goschler <git@nce.wtf>
2025-08-21 11:36:14 +0200

    rename k9s plugins

--- profiles/applications/kubernetes.nix
+++ profiles/applications/kubernetes.nix
@@ -51,43 +51,40 @@ with lib;
     k9s = {
       enable = true;
 
-      plugin = {
-        plugins = {
+      plugins = {
 
-          stern = {
-            shortCut = "Shift-L";
-            confirm = false;
-            description = "Logs <Stern>";
-            scopes = [ "pods" ];
-            command = "stern";
-            background = false;
-            args = [
-              "--tail"
-              "50"
-              "$FILTER"
-              "-n"
-              "$NAMESPACE"
-              "--context"
-              "$CONTEXT"
-            ];
-          };
-
-          reconcile-hr = {
-            shortCut = "Shift-R";
-            confirm = false;
-            description = "Flux reconcile";
-            scopes = [
-              "helmreleases"
-            ];
-            command = "bash";
-            background = false;
-            args = [
-              "-c"
-              "flux --context $CONTEXT reconcile helmrelease -n $NAMESPACE $NAME |& less"
-            ];
-          };
+        stern = {
+          shortCut = "Shift-L";
+          confirm = false;
+          description = "Logs <Stern>";
+          scopes = [ "pods" ];
+          command = "stern";
+          background = false;
+          args = [
+            "--tail"
+            "50"
+            "$FILTER"
+            "-n"
+            "$NAMESPACE"
+            "--context"
+            "$CONTEXT"
+          ];
         };
 
+        reconcile-hr = {
+          shortCut = "Shift-R";
+          confirm = false;
+          description = "Flux reconcile";
+          scopes = [
+            "helmreleases"
+          ];
+          command = "bash";
+          background = false;
+          args = [
+            "-c"
+            "flux --context $CONTEXT reconcile helmrelease -n $NAMESPACE $NAME |& less"
+          ];
+        };
       };
 
       settings = {
